a5b6b24c39e08d83eccf281261e7c87ae777dc54
Milancho <milancho.arsovski@gmail.com>
2018-04-06 11:26:02 +0200

    Add Users Controller

---
+++ DemoApplication/DemoWebApi/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoWebApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok("Users");
+        }
+    }
+}
