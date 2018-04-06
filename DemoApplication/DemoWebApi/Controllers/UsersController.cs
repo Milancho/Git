987a7c6588cdb96dd05ffe32adeda28153b2770d
Milancho <milancho.arsovski@gmail.com>
2018-04-06 17:19:06 +0200

    List of Users

--- DemoApplication/DemoWebApi/Controllers/UsersController.cs
+++ DemoApplication/DemoWebApi/Controllers/UsersController.cs
@@ -9,6 +9,10 @@ namespace DemoWebApi.Controllers
 {
     public class UsersController : ApiController
     {
+        /// <summary>
+        /// List of Users
+        /// </summary>
+        /// <returns></returns>
         public IHttpActionResult Get()
         {
             return Ok("User");
