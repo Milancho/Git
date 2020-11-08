c56e33eb97f9abea8bb8cd0dd1458d81d5a8cfee
Milancho Arsovski <milancho.arsovski@outlook.com>
2020-11-08 10:51:34 +0100

    movies data anotation

--- AdminLTE.MVC/AdminLTE.MVC/Models/Movie.cs
+++ AdminLTE.MVC/AdminLTE.MVC/Models/Movie.cs
@@ -13,9 +13,12 @@ namespace AdminLTE.MVC.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
