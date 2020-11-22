87369b3bb59ef5798a2d32c164d7a55eba285a99
Milancho Arsovski <milancho.arsovski@outlook.com>
2020-11-22 18:19:29 +0100

    Startup refactoring

--- SpeechTherapist/SpeechTherapist.Web/Startup.cs
+++ SpeechTherapist/SpeechTherapist.Web/Startup.cs
@@ -24,7 +24,6 @@ namespace SpeechTherapist.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,7 +55,6 @@ namespace SpeechTherapist.Web
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-
             });
         }
     }
