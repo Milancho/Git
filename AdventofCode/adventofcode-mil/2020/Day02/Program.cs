5853e5b08ba2ee71d815ea82d9272d51bd85f2a1
Milancho Arsovski <milancho.arsovski@outlook.com>
2020-12-05 11:15:29 +0100

    day2 complete

--- AdventofCode/adventofcode-mil/2020/Day02/Program.cs
+++ AdventofCode/adventofcode-mil/2020/Day02/Program.cs
@@ -10,10 +10,12 @@ namespace Day02
         static void Main(string[] args)
         {
             Console.WriteLine("Solution1");
-            Solution1();
+            Solution1_PartOne();
+            Console.WriteLine("Solution2");
+            Solution1_PartTwo();
         }
 
-        private static void Solution1()
+        private static void Solution1_PartOne()
         {
             var i = 0;
             foreach (var line in Lines())
@@ -29,7 +31,7 @@ namespace Day02
                 var most = leastMost[1];
 
                 var letter = part2[0];
-                var counter = password.Count(c => c == letter);            
+                var counter = password.Count(c => c == letter);
 
                 if (counter >= least && counter <= most)
                 {
@@ -39,9 +41,37 @@ namespace Day02
             Console.WriteLine(i);
         }
 
+        private static void Solution1_PartTwo()
+        {
+            var i = 0;
+            foreach (var line in Lines())
+            {
+                var s3 = line.Split(' ');
+
+                var policyPolicy = s3[0];
+                var part2 = s3[1];
+                var password = s3[2];
+
+                var policyList = policyPolicy.Split('-').Select(int.Parse).ToList();
+                var posOne = policyList[0];
+                var posThree = policyList[1];
+
+                var letter = part2[0];
+
+                var firstMatch = letter == password[posOne - 1];
+                var secondMatch = letter == password[posThree - 1];
+
+                if ((firstMatch || secondMatch) && (firstMatch != secondMatch))
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine(i);
+        }
+
         static string[] Lines()
         {
-            string text = System.IO.File.ReadAllText(@"Day02\input.in"); // 
+            string text = System.IO.File.ReadAllText(@"Day02\input.in"); 
             return text.Split('\n');
         }
 
